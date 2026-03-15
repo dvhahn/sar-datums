96da0ea4235eb7f21a4e018d1098096a89b0802a
David Hahn <232964849+dvhahn@users.noreply.github.com>
2026-03-15 23:19:46 +1300

    add: template drift class to fomulate VBA

---
+++ services/drift.py
@@ -0,0 +1,17 @@
+import math
+from domain.model import Coordinate, Wind, SearchObject, CurrentVector
+
+KNOTS_TO_MS = 1852 / 3600
+METRES_PER_DEGREE_LAT = 111120
+
+def calculate_leeway():
+    # TODO
+    pass
+
+def apply_drift_step():
+    # TODO
+    pass
+
+def get_tidal_current():
+    # TODO
+    pass
