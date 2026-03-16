47d67bcc0043e9ece0e3320e045701a906af111b
David Hahn <232964849+dvhahn@users.noreply.github.com>
2026-03-16 17:33:28 +1300

    implement: apply drift step function

--- services/drift.py
+++ services/drift.py
@@ -14,17 +14,32 @@ def calculate_leeway(wind: Wind, search_object: SearchObject) -> CurrentVector:
     else:
         wind_speed = 0
 
-    drift_direction = (wind.direction_deg + 180) % 360
-    vx = math.sin(math.radians(wind.direction_deg)) * wind_speed
-    vy = math.cos(math.radians(wind.direction_deg)) * wind_speed
+    drift_direction = (wind.direction_deg + 180) % 360 # reverse. 0 degree - north, when wind comes from north object will be pushed south.
+    vx = math.sin(math.radians(drift_direction)) * wind_speed
+    vy = math.cos(math.radians(drift_direction)) * wind_speed
 
     return CurrentVector(vx, vy)
 
+def get_tidal_current(lat: float, lon: float, time) -> CurrentVector:
+    """retrieve tidal data from DB, to be implemented after DB.
+        See "Find" function form Data sheet.
+    """
 
-def apply_drift_step():
-    # TODO
-    pass
+    return CurrentVector(0, 0)
 
-def get_tidal_current():
-    # TODO
-    pass
+
+def apply_drift_step(position: Coordinate, current: CurrentVector, leeway: CurrentVector, time_delta_seconds: float) -> Coordinate:
+    """
+        this function will calculate the coordinate of an object drifting every 0.1 hour.
+        adds leeway + tidal -> convert into lat, lon.
+    """
+    total_vx = current.vx + leeway.vx
+    total_vy = current.vy + leeway.vy
+
+    dx = total_vx * time_delta_seconds  # metres
+    dy = total_vy * time_delta_seconds  # metres
+
+    new_lat = position.lat + (dy / 111120)
+    new_lon = position.lon + ((dx / 111120) / math.cos(math.radians(position.lat)))
+
+    return Coordinate(new_lat, new_lon)
