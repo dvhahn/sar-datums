1d64d4ef44f3e0507215f52cef48dfdd1b883547
Bobby <byar960@aucklanduni.ac.nz>
2026-03-19 11:39:16 +1300

    generate gpx completed for now

--- services/gpx.py
+++ services/gpx.py
@@ -1,4 +1,30 @@
 from domain.model import Coordinate
 
 def generate_gpx(positions: list[Coordinate], name: str) -> str:
-    pass
+    gpx = []
+
+    gpx.append('<?xml version="1.0" encoding="UTF-8"?>')
+    gpx.append('<gpx version="1.1" creator="Drift Simulator" xmlns="http://www.topografix.com/GPX/1/1">')
+
+    # add track
+    gpx.append(f"<trk>")
+    gpx.append(f"<name>{name}</name>")
+
+    # add segment
+    gpx.append("<trkseg>")
+
+    # add point (lat,lon)
+    for pos in positions:
+        gpx.append(f'<trkpt lat="{pos.lat}" lon="{pos.lon}"></trkpt>')
+
+
+    gpx.append("</trkseg>")
+    gpx.append("</trk>")
+    gpx.append("</gpx>")
+
+    return "\n".join(gpx)
+
+
+
+
+
